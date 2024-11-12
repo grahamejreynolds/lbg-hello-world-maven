8a3da4617b5ef8bd63168c17e25d20be4771f4f6
Grahame <grahame@grahame.com>
2024-11-12 14:03:32 +0000

    Aded Donald

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Donald"));
         System.out.println(sayHelloToSomeone("Grahame R"));
         System.out.println(sayGoodbye());
     }
