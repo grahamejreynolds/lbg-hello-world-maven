304bf5d62984057a77e41be27df27404919d71b1
Grahame <grahame@grahame.com>
2024-11-12 13:58:22 +0000

    added my name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Grahame"));
+        System.out.println(sayHelloToSomeone("Grahame R"));
         System.out.println(sayGoodbye());
     }
 
